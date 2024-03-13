85275bc204bf52c48c28745e79321cf98bd73694
trashpenguin <francisninoranile.moncada@bicol-u.edu.ph>
2024-03-14 04:15:03 +0800

    fen fuction board.c

--- defs.h
+++ defs.h
@@ -41,6 +41,7 @@ typedef unsigned long long U64;
 #define NAME "vice 1.0"
 #define BRD_SQ_NUM 120
 #define MAXGAMEMOVES 2048
+#define START_FEN	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 
 enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
